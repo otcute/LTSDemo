f50a8f8a153a76b90ede39a248f17ba1d184ada6
Bùi Hiếu <buichihieu1321999@gmail.com>
2023-02-08 12:20:10 +0700

    fix

--- dj-client/src/component/Table/TableComponent.tsx
+++ dj-client/src/component/Table/TableComponent.tsx
@@ -5,11 +5,11 @@ function TableComponent() {
     return (
         <div style={{ padding: '24px 32px' }}>
             <div className="table-title">
-                <h3>Học Viên</h3>
+                <h3>Thống kê</h3>
             </div>
             <Table striped bordered hover responsive="sm">
                 <thead>
-                    <tr>
+                    <tr style={{ backgroundColor: '#ccc' }}>
                         <th>Họ tên</th>
                         <th>Khóa học</th>
                         <th>Ngày vào</th>
@@ -20,7 +20,7 @@ function TableComponent() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Bùi Chí Hiếu</td>
+                        <td style={{ cursor: 'pointer' }}>Bùi Chí Hiếu</td>
                         <th>Front-End</th>
                         <th>1/8/2022</th>
                         <th>1/10/2022</th>
