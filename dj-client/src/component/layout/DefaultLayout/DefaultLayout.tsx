a9af3e1b15089156bf659c1051adc96bcdce0365
dj-xuyenchi <do.quanganh99zz@gmail.com>
2023-02-08 13:21:53 +0700

    update

--- dj-client/src/component/layout/DefaultLayout/DefaultLayout.tsx
+++ dj-client/src/component/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import Header from '../components/Header';
+import Header from '../components/Header/Header';
 import SideBar from '../components/Sidebar/SideBar';
 import TableComponent from '../../Table/TableComponent';
 import './DefaultLayout.scss';
